7af6ff27abcbb174637e5d4ddefc411d84e6667f
Behruz <shonazarovbehruz426@gmail.com>
2026-08-01 11:32:52 +0500

    fix: remove --no-sandbox flag causing warning

--- start-chrome.sh
+++ start-chrome.sh
@@ -2,9 +2,11 @@
 export DISPLAY=:1
 
 # 1-profil (Default)
-google-chrome --profile-directory="Default" --no-sandbox > /dev/null 2>&1 &
+google-chrome --profile-directory="Default" > /dev/null 2>&1 &
+
+sleep 2
 
 # 2-profil (Profile 1)
-google-chrome --profile-directory="Profile 1" --no-sandbox > /dev/null 2>&1 &
+google-chrome --profile-directory="Profile 1" > /dev/null 2>&1 &
 
 echo "Ikkala profil ham VNC ekranida ishga tushdi!"
